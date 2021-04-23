c45c79e9608efba8a34e12e8dd235f0d2dee9a81
gfunkmaster <nurkicelmin@hotmail.com>
2021-04-23 19:05:45 +0200

    2. can open the navbar and move content

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 
+const navTriggerEl = document.querySelector('.hamburger');
+const navEl = document.querySelector('.nav')
+const contentEl = document.querySelector('.content')
+const hamburgerBarsEL = document.getElementsByTagName('span')
+
+const toggleNav = () => {
+    
+    navTriggerEl.addEventListener('click', () =>{
+        navEl.classList.add('open')
+        contentEl.classList.add('shift')
+    })
+}
+
+toggleNav()

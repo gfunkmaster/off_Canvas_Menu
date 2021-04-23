dda3e92d83774a8ee64c3f8865bd752c726f5bb6
gfunkmaster <nurkicelmin@hotmail.com>
2021-04-23 19:19:58 +0200

    3. added toggle for menu, and change beewtween bars and x

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
-
-const navTriggerEl = document.querySelector('.hamburger');
-const navEl = document.querySelector('.nav')
-const contentEl = document.querySelector('.content')
-const hamburgerBarsEL = document.getElementsByTagName('span')
+const navTriggerEl = document.querySelector(".hamburger");
+const navEl = document.querySelector(".nav");
+const contentEl = document.querySelector(".content");
+const hamburgerBarsEL = document.getElementsByTagName("span");
 
 const toggleNav = () => {
-    
-    navTriggerEl.addEventListener('click', () =>{
-        navEl.classList.add('open')
-        contentEl.classList.add('shift')
-    })
-}
+  /* trigger the click, and toggle the menu and content */
+  navTriggerEl.addEventListener("click", () => {
+    navEl.classList.toggle("open");
+    contentEl.classList.toggle("shift");
 
-toggleNav()
+    addAnimationToBars();
+  });
+};
+
+//add change class to hamburgare menu and toggle it
+const addAnimationToBars = () => {
+  for (let item of hamburgerBarsEL) {
+    console.log(item);
+    item.classList.toggle("change");
+  }
+};
+
+toggleNav();
